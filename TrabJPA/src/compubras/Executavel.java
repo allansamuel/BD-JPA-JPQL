f9fc0096df9e3ab9f5002d72b6ae32f56c20f000
allansamuel <allangomeseu@hotmail.com>
2019-11-09 18:25:28 -0300

    3questao jpql

--- TrabJPA/src/compubras/Executavel.java
+++ TrabJPA/src/compubras/Executavel.java
@@ -20,7 +20,7 @@ public class Executavel {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		
-		buscarQuantidadePorPedido(em);
+		buscarProdutosVendidos(em);
 		
 		em.getTransaction().commit();
 		em.close();
@@ -32,16 +32,32 @@ public class Executavel {
 				+ "LEFT JOIN Pedido AS p ON p.cliente.codCliente = c.codCliente", Object[].class);
 		List<Object[]> results = query.getResultList();
 		int calc = 0;
-//		for (Object[] result : results) {
-//			calc++;
-//			System.out.println("Cliente: " + result[0] + ", Pedido: " + result[1]);
-//		}
-//		System.out.println(calc);
+		for (Object[] result : results) {
+			calc++;
+			System.out.println("Cliente: " + result[0] + ", Pedido: " + result[1]);
+		}
+		System.out.println(calc);
+		return results;
+	}
+	
+	
+//	Mostre o código do produto, a descrição e o valor total obtido por cada produto ao longo da história da loja. 
+//	Ordene a lista pelo valor total dos produtos. Observe que mesmo os produtos que nunca foram vendidos devem ser exibidos.
+	public static List<Object[]> buscarProdutosVendidos(EntityManager em) {
+		TypedQuery<Object[]> query = em.createQuery("select p.codProduto, p.descricao, sum(coalesce(i.quantidade,0) * p.valorUnitario) as obtido " + 
+				"from Produto p left join ItemPedido i on i.produto.codProduto = p.codProduto group by p.codProduto", Object[].class);
+		List<Object[]> results = query.getResultList();
+		int calc = 0;
+		for (Object[] result : results) {
+			calc++;
+			System.out.println("CodProduto: " + result[0] + ", Descricao: " + result[1] + ", ValorObtido: " + result[2]);
+		}
+		System.out.println(calc);
 		return results;
 	}
 	
 	//Liste o nome do cliente, o código do pedido e a quantidade total de produtos por pedido.
-	public static void buscarQuantidadePorPedido(EntityManager em) {
+	public static List<Object[]> buscarQuantidadePorPedido(EntityManager em) {
 		TypedQuery<Object[]> query = em.createQuery("SELECT c.nome, p.codPedido, SUM(COALESCE(i.quantidade,0)) FROM Cliente AS c " + 
 				"LEFT JOIN Pedido AS p ON p.cliente.codCliente = c.codCliente " + 
 				"LEFT JOIN ItemPedido AS i ON p.codPedido = i.pedido.codPedido " + 
@@ -53,7 +69,7 @@ public class Executavel {
 			System.out.println("Cliente: " + result[0] + ", Pedido: " + result[1] + ", Soma: " + result[2]);
 		}
 		System.out.println(calc);
-//		return results;
+		return results;
 	}
 
 }
