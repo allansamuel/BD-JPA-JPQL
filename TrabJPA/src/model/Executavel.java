e5d31fc440cd4c3bbae7680c5ed7e3c4548897e8
allansamuel <allangomeseu@hotmail.com>
2019-11-09 21:30:38 -0300

    Finalizando 2questao JPQL

--- TrabJPA/src/model/Executavel.java
+++ TrabJPA/src/model/Executavel.java
@@ -13,18 +13,18 @@ public class Executavel {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		
-
-		TypedQuery<ClienteEPedido> query = em.createQuery("SELECT new model.ClienteEPedido(p.cliente.nome, p.codPedido) FROM Cliente AS c "
-				+ "INNER JOIN Pedido AS p ON p.cliente.codCliente = c.codCliente "
-				+ "ORDER BY c.nome, p.codPedido", ClienteEPedido.class);
-		List<ClienteEPedido> results = query.getResultList();
+		TypedQuery<ClienteEPedidoEProdutos> query = em.createQuery("SELECT new model.ClienteEPedidoEProdutos" + 
+				"(c.nome, p.codPedido, SUM(COALESCE(i.quantidade,0))) FROM Cliente AS c " + 
+				"INNER JOIN Pedido AS p ON p.cliente.codCliente = c.codCliente " + 
+				"LEFT JOIN ItemPedido AS i ON p.codPedido = i.pedido.codPedido " + 
+				"GROUP BY p.codPedido ORDER BY c.codCliente, p.codPedido", ClienteEPedidoEProdutos.class);
+		List<ClienteEPedidoEProdutos> results = query.getResultList();
 		int calc = 0;
-		for (ClienteEPedido result : results) {
+		for (ClienteEPedidoEProdutos result : results) {
 			calc++;
-			System.out.println("Cliente: " + result.getNome() + ", Pedido: " + result.getCodPedido());
+			System.out.println("Cliente: " + result.getNome() + ", Pedido: " + result.getCodPedido() + ", Quantia: " + result.getQuantidade());
 		}
 		System.out.println(calc + " rows");
-		
 		
 		
 		em.getTransaction().commit();
@@ -32,17 +32,20 @@ public class Executavel {
 		emf.close();
 	}
 }
-class ClienteEPedido {
+class ClienteEPedidoEProdutos {
 		private String nome;
 		private Integer codPedido;
+		private long quantidade;
 		
-		public ClienteEPedido() {
+		public ClienteEPedidoEProdutos() {
 			super();
 		}
 
-		public ClienteEPedido(String nome, Integer codPedido) {
-		this.nome = nome;
-		this.codPedido = codPedido;
+		public ClienteEPedidoEProdutos(String nome, Integer codPedido, long quantidade) {
+			super();
+			this.nome = nome;
+			this.codPedido = codPedido;
+			this.quantidade = quantidade;
 		}
 
 		public String getNome() {
@@ -60,6 +63,16 @@ class ClienteEPedido {
 		public void setCodPedido(Integer codPedido) {
 			this.codPedido = codPedido;
 		}
+
+		public long getQuantidade() {
+			return quantidade;
+		}
+
+		public void setQuantidade(long quantidade) {
+			this.quantidade = quantidade;
+		}
+
+		
 
 		
 	}
